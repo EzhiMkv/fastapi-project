66305d5261e221a08d78f6d79f1fe4d5e1c1eda1
e <e.malkov.86@mail.ru>
2024-06-07 00:27:15 +0500

    start developing

---
+++ backend/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from core.config import settings
+from db.session import engine
+from db.base_class import Base
+
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+    create_tables()
+    return app
+
+
+app = start_application()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
